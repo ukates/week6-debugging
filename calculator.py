819736cea8f26b425aeb6e9a12898b2d8544a8af
Derrick Cates <dcates@mail.valenciacollege.edu>
2016-02-15 15:52:11 -0500

    Week 6 try 1

--- calculator.py
+++ calculator.py
@@ -1,17 +1,25 @@
 def add( first, second):
     # TODO:
     # there's an error in this code, fix it
-    return first plus second
+    return int(first) + int(second)
 
 def subtract( first, second):
+    return int(first) - int(second)
     # TODO:
     # fill in code here that will return the difference between first and second
 
 def multiply( first, second):
+    return int(first) * int(second)
     # TODO:
     # fill in code here that will return the product of first and second
 
 def divide( first, second):
+    if second == int(0):
+        raise Exception ("I'm sorry, I can't divide by zero")
+    return int(first) / int(second)
+
+
+    
     # TODO:
     # fill in code here that:
     #   1. checks the second number to see if it is zero
